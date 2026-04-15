6e64f04a45e60534192d29769d789648155947ed
KhoirulHS <wibuadalahrasterkuat@gmail.com>
2026-04-16 01:38:42 +0700

    fix: finite automata and finite automata edge

--- src/ruldani_scanner_lexer/utils/finite_automata/finite_automata.py
+++ src/ruldani_scanner_lexer/utils/finite_automata/finite_automata.py
@@ -1,21 +1,40 @@
+from ruldani_scanner_lexer.utils.finite_automata.finite_automata_edge import finite_automata_edge
+
 class finite_automata:
     """
     finite automata node merupakan sebuah node yang berisi state 
     pada finite automata. Finite automata dilambangkan dengan lingkaran.
     node atau state ini memiliki sejumlah transisi yang dilambangkan dengan panah.
     """
-    def __init__(self,  nama_node: str, 
-                        transisi: list,
-                         ):
+    def __init__(self, name: int):
         
-        print("inisialisasi finite automata node berhasil")
         self.expression = None
-        self.next = None
+        self.next: list[finite_automata_edge] = None
+        self.accepting: bool = False
+        self.start_state: bool = False
     
-    def compile_transisi(self):
+    def make_transition(self, edge: finite_automata_edge):
+        self.next.append(edge)
         return None
+    
+    def set_accepting(self) -> None:
+        self.accepting = True
+        return None
+    
+    def is_accepting(self) -> None:
+        return self.accepting
+    
+    def set_start_state(self) -> None:
+        self.start_state = True
+        return None
+    
+    def is_start_state(self) -> None:
+        return self.start_state
 
-    def print_node(self):
+    def eval_finite_automata_edge(self, char: str):
+        for finite_edge in self.next :
+            if(finite_edge.evaluate(char)):
+                return finite_edge.next_finite_automata()
         return None
 
     def next_node(self, node_s):
--- src/ruldani_scanner_lexer/utils/finite_automata/finite_automata_edge.py
+++ src/ruldani_scanner_lexer/utils/finite_automata/finite_automata_edge.py
@@ -1,7 +1,18 @@
+from ruldani_scanner_lexer.utils.finite_automata.finite_automata import finite_automata
+
 class finite_automata_edge:
     """
-    finite auromata arrow merupakan sebuah diantara state. finite automata ini dapat
-    menunjuk dirinya sendiri.
+    finite auromata arrow edge digunakan untuk menunjuk state berikutnya. Finite automata edge dapat berisi beberapa method
     """
-    def __init__(self):
-        pass
+    def __init__(self, edge: str, next_node: finite_automata):
+        self.next_node: finite_automata = next_node
+        self.edge: str = edge
+
+    def evaluate(self, string_a: str) -> bool:
+        if self.edge == string_a:
+            return True
+        
+        return False
+    
+    def next_finite_automata(self) -> finite_automata :
+        return self.next_node
